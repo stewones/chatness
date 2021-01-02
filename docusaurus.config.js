70573167e903a991c1d53a4a9e39f81306505a65
Stewan <oi@stewan.io>
2021-01-02 03:27:01 -0300

    chore(): add search

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,18 +8,17 @@ module.exports = {
   projectName: 'chatness',
   stylesheets: [],
   themeConfig: {
-    disableDarkMode: false,
+    colorMode: { disableSwitch: false },
     prism: {
       theme: require('./src/js/monokaiTheme.js'),
     },
     navbar: {
       title: 'Chatness',
-      image: 'images/chatness-logo.svg',
       logo: {
         alt: 'Chatness',
         src: 'images/chatness-logo.svg',
       },
-      links: [
+      items: [
         {
           label: 'Getting Started',
           to: 'getting-started',
@@ -146,11 +145,6 @@ module.exports = {
       },
       copyright: `<small><span>©${new Date().getFullYear()}</span><br />handcrafted by <a href="https://twitter.com/stw" rel="noopener" target="_blank">Stw</a></small>`,
     },
-    algolia: {
-      apiKey: 'asdf',
-      indexName: 'chatness',
-      algoliaOptions: {},
-    },
     gtag: {
       trackingID: 'G-CVQ2BYVZLS',
     },
@@ -166,6 +160,53 @@ module.exports = {
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+  plugins: [
+    [
+      require.resolve('@cmfcmf/docusaurus-search-local'),
+      {
+        // whether to index docs pages
+        indexDocs: true,
+        // must start with "/" and correspond to the routeBasePath configured for the docs plugin
+        // use "/" if you use docs-only-mode
+        // (see https://v2.docusaurus.io/docs/2.0.0-alpha.70/docs-introduction#docs-only-mode)
+        docsRouteBasePath: '/docs',
+
+        // Whether to also index the titles of the parent categories in the sidebar of a doc page.
+        // 0 disables this feature.
+        // 1 indexes the direct parent category in the sidebar of a doc page
+        // 2 indexes up to two nested parent categories of a doc page
+        // 3...
+        //
+        // Do _not_ use Infinity, the value must be a JSON-serializable integer.
+        indexDocSidebarParentCategories: 2,
+
+        // whether to index blog pages
+        indexBlog: false,
+        // must start with "/" and correspond to the routeBasePath configured for the blog plugin
+        // use "/" if you use blog-only-mode
+        // (see https://v2.docusaurus.io/docs/2.0.0-alpha.70/blog#blog-only-mode)
+        blogRouteBasePath: '/blog',
+
+        // whether to index static pages
+        // /404.html is never indexed
+        indexPages: true,
+
+        // language of your documentation, see next section
+        language: 'en',
+
+        // lunr.js-specific settings
+        lunr: {
+          // When indexing your documents, their content is split into "tokens".
+          // Text entered into the search box is also tokenized.
+          // This setting configures the separator used to determine where to split the text into tokens.
+          // By default, it splits the text at whitespace and dashes.
+          //
+          // Note: Does not work for "ja" and "th" languages, since these use a different tokenizer.
+          tokenizerSeparator: /[\s\-]+/,
         },
       },
     ],
